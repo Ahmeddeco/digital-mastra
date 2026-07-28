7d13609cc9a932444d9d59182a2bf739afa907e2
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2026-07-28 19:01:00 +0300

    fix some issues

--- src/app/[locale]/server/members/edit/[id]/page.tsx
+++ src/app/[locale]/server/members/edit/[id]/page.tsx
@@ -1,18 +1,18 @@
 import { isAllowedRoles } from "@/auth/isAllowedRoles"
 import ServerPageCard from "@/components/backend/ServerPageCard"
 import { getAllClientsForSelect } from "@/dl/clients.data"
-import { getOneClientMember } from "@/dl/member.data"
+import { getOneMember } from "@/dl/member.data"
 import { getAllUsersForSelect } from "@/dl/users.data"
 import EditMember from "@/forms/EditMember"
 import { Role } from "@/generated/prisma/enums"
 import { getAllClientsForSelectType } from "@/types/client.type"
-import { getOneClientMemberType } from "@/types/member.type"
+import { getOneMemberType } from "@/types/member.type"
 import { getAllUsersForSelectType } from "@/types/user.type"
 
 export default async function AddMemberPage({ params }: { params: Promise<{ id: string }> }) {
 	await isAllowedRoles([Role.admin])
 	const id = (await params).id
-	const member: getOneClientMemberType = await getOneClientMember(id)
+	const member: getOneMemberType = await getOneMember(id)
 	const allUsers: getAllUsersForSelectType = await getAllUsersForSelect()
 	const allClients: getAllClientsForSelectType = await getAllClientsForSelect()
 
